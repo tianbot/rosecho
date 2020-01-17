18a1c438c30ab1c6d892ef836aa0bffff7824725
tianb03 <tianb03@hotmail.com>
2020-01-17 16:17:08 +0800

    add udev rules for rosecho

---
+++ install/install_udev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "remap the device serial port(ttyUSBX) to rosecho"
+echo "check it using the command : ls -l /dev|grep ttyUSB"
+
+sudo cp ./_udev_/99-tianbot-rosecho.rules  /etc/udev/rules.d
+
+echo " "
+echo "Restarting udev"
+echo ""
+sudo service udev reload
+sudo service udev restart
+
