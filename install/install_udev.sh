37e4674b5101548ff2ed3f7fcfff1607c5e1ba8b
tianb03 <tianb03@hotmail.com>
2020-10-26 15:56:10 +0800

    udev rules updated to fix bug in 18.04

--- install/install_udev.sh
+++ install/install_udev.sh
@@ -3,6 +3,7 @@
 echo "remap the device serial port(ttyUSBX) to rosecho"
 echo "check it using the command : ls -l /dev|grep ttyUSB"
 
+sudo rm /etc/udev/rules.d/99-tianbot-rosecho.rules
 sudo cp ./_udev_/99-tianbot-rosecho.rules  /etc/udev/rules.d
 
 echo " "
