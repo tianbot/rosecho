8f7b856a0aa62605242790abb47b3889dbabbcdd
tianb03 <tianb03@hotmail.com>
2020-12-05 17:35:10 +0800

    voice teleop init

--- scripts/voice_teleop.py
+++ scripts/voice_teleop.py
@@ -22,58 +22,42 @@ from std_msgs.msg import Int16, String
 
 
 class RosEchoVoiceTeleop():
-    def asr_callback(self, data):
+    def answer_callback(self, data):
         ros_msg = data.data.strip('"')
         if not isinstance(ros_msg, unicode):
             ros_msg = unicode(ros_msg, "utf8")
+        
         linear_vel   = 0.0
         angular_vel  = 0.0
 
         if ros_msg == u"前进":
             self.rosecho_tts.cancel_goal()
-
-            text = ttsGoal()
-            text.text = "前进！"
-            self.speak(text)
             
             linear_vel = 0.3
 
-        if ros_msg == u"后退":
+        elif ros_msg == u"后退":
             self.rosecho_tts.cancel_goal()
-
-            text = ttsGoal()
-            text.text = "后退！"
-            self.speak(text)
 
             linear_vel = -0.3
 
-        if ros_msg == u"左转":
+        elif ros_msg == u"左转":
             self.rosecho_tts.cancel_goal()
-
-            text = ttsGoal()
-            text.text = "左转！"
-            self.speak(text)
 
             angular_vel = 0.3
 
-        if ros_msg == u"右转":
+        elif ros_msg == u"右转":
             self.rosecho_tts.cancel_goal()
-
-            text = ttsGoal()
-            text.text = "右转！"
-            self.speak(text)
 
             angular_vel = -0.3
         
-        if ros_msg == u"停止":
+        elif ros_msg == u"停止":
             self.rosecho_tts.cancel_goal()
 
-            text = ttsGoal()
-            text.text = "停止！"
-            self.speak(text)
             linear_vel = 0.0
             angular_vel = 0.0  
-  
+        else:
+            return
+
         twist = Twist()
         twist.linear.x = linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
         twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = angular_vel
@@ -94,8 +78,8 @@ class RosEchoVoiceTeleop():
         # Send the goal pose to the tts server  
         self.rosecho_tts.send_goal(text)
         rospy.loginfo(text.text);  
-        # Allow 1 minute to speak  
-        finished_within_time = self.rosecho_tts.wait_for_result(rospy.Duration(5))   
+        # Allow 30 sec to speak  
+        finished_within_time = self.rosecho_tts.wait_for_result(rospy.Duration(30))   
         # If we don't finish in time, abort the goal  
         if not finished_within_time:  
             self.rosecho_tts.cancel_goal()  
@@ -121,7 +105,7 @@ class RosEchoVoiceTeleop():
         # stop robot when exit
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
 
-        rospy.Subscriber('/rosecho/asr', String, self.asr_callback)
+        rospy.Subscriber('/rosecho/answer', String, self.answer_callback)
 
 
 if __name__ == "__main__":
